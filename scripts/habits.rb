acfa61e11263f8ecf3f838468b0ae89b8f7482b0
Kurt Funai <jesuiskurt@gmail.com>
2013-04-08 18:21:35 -0400

    Now calculating productive days/months in the habits.rb script. Removed date reformating from habits.rb file.

--- scripts/habits.rb
+++ scripts/habits.rb
@@ -13,19 +13,45 @@ habits_source.close
 
 # Write each habit into an individual json file
 habits.each do |habit|
-  # Format the dates to trim time/timezone information
-  habit['createdAt'] = Date.parse(habit['createdAt']).strftime('%F')
-  habit['completed'].each_index do |date_completed|
-    habit['completed'][date_completed] = Date.parse(habit['completed'][date_completed]).strftime('%F')
+
+  # Init our productive days/months hashes
+  habit['productiveDays'] = {
+    'Monday' => 0,
+    'Tuesday' => 0,
+    'Wednesday' => 0,
+    'Thursday' => 0,
+    'Friday' => 0,
+    'Saturday' => 0,
+    'Sunday' => 0
+  }
+  habit['productiveMonths'] = {
+    'January' => 0,
+    'February' => 0,
+    'March' => 0,
+    'April' => 0,
+    'May' => 0,
+    'June' => 0,
+    'July' => 0,
+    'August' => 0,
+    'September' => 0,
+    'October' => 0,
+    'November' => 0,
+    'December' => 0
+  }
+
+  # Get times completed for each weekday & month
+  habit['completed'].each do |date_completed|
+    date = Date.parse(date_completed)
+    habit['productiveDays'][date.strftime('%A')] += 1 # %A => Full day name, ie. "Tuesday"
+
+    # Only calculate productive months for the last year, to prevent skewed results
+    habit['productiveMonths'][date.strftime('%B')] += 1 if date > Date.today.prev_year #B => Full month name, ie. "April"
   end
-  File.open "app/habits/#{habit['name'].downcase}.json", 'w' do |f|
+
+  # Save habit details in a json file
+  File.open 'app/habits/#{habit["name"].downcase}.json', 'w' do |f|
     f.write JSON.pretty_generate(habit)
   end
 end
 
-# Overwrite the habits.json file with formatted habits
-File.open "app/habits/habits.json", 'w' do |f|
-  f.write JSON.pretty_generate(habits)
-end
-
-puts "--- Finished writing json files"
+puts '--- Completed writing habit files'
